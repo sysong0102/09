48b4025de96f2cbb99d410e286893a2b07c0da7c
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-05 03:51:48 +0900

    Q3-2

--- main.c
+++ main.c
@@ -8,18 +8,21 @@
 int main(int argc, char *argv[]) 
 {
 	int i;
-    int score[N_STUDENT];
-    int grade[N_STUDENT];
+    int a[N_STUDENT] = {1, 2, 3, 4, 5};
+    int b[N_STUDENT] = {1, 2, 3, 4, 6};
+    int flag = 0;
     
 	for (i=0;i<N_STUDENT;i++)
-		grade[i] = rand()%100 + 1;
-    
-	for (i=0;i<N_STUDENT;i++)
-		score[i] = grade[i];
-		
-	for (i=0;i<N_STUDENT;i++)
-       printf("score[%d] : %d (grade : %i)\n", i, score[i], grade[i]);
-   
+    {
+		if (a[i] != b[i])
+		{
+      		printf("array a and b are not the same\n");
+   	    	flag = 1;
+		}
+	}
+	if (flag == 0)
+        	printf("array a and b are the same\n"); 
+	
 	system("PAUSE");
 	return 0;
 }
