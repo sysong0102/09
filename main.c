4db14bf32c5a8b86935763535aa205450a85247a
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-05 03:40:51 +0900

    Q2-2

--- main.c
+++ main.c
@@ -7,15 +7,20 @@
 
 int main(int argc, char *argv[]) 
 {
-	int grade[N_STUDENT];
-	int i;
-	printf("input 5 scores : ");
-	
-	for (i=0;i<N_STUDENT;i++)
-		scanf("%d", &grade[i]);
-	for (i=0;i<N_STUDENT;i++)
-		printf("grade[%i] = %i\n", i, grade[i]);
-	
+	int i, average;
+    int sum;
+    int grade[N_STUDENT];
+    printf("input 5 scores:");
+   
+    sum = 0;
+    for (i=0;i<N_STUDENT;i++)
+    {
+       scanf("%d", &grade[i]);
+       sum += grade[i];
+    }
+       average = sum / N_STUDENT;
+       printf("score average : %i\n", average);
+   
 	system("PAUSE");
 	return 0;
 }
